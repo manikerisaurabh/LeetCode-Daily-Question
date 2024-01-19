ceffa8e57d4ecc53d622c8501534f28affcbc53e
manikerisaurabh <manikerisaurabh@gmail.com>
2024-01-19 22:13:34 +0530

    19-Jan :+1:

---
+++ 2024/Jan-2024/19-Jan.java
@@ -0,0 +1,72 @@
+/*
+====================================================== 931. Minimum Falling Path Sum ======================================================
+
+
+Problem Statement : Given an n x n array of integers matrix, return the minimum sum of any falling path through matrix.
+
+                    A falling path starts at any element in the first row and chooses the element in the next row that is either directly below or diagonally left/right. Specifically, the next element from position (row, col) will be (row + 1, col - 1), 
+            
+                    (row + 1, col), or (row + 1, col + 1).
+
+
+Examples : 
+
+                    Example 1:
+                    Input: matrix = [[2,1,3],[6,5,4],[7,8,9]]
+                    Output: 13
+                    Explanation: There are two falling paths with a minimum sum as shown.
+
+
+                    Example 2:
+                    Input: matrix = [[-19,57],[-40,-5]]
+                    Output: -59
+                    Explanation: The falling path with a minimum sum is shown.
+*/
+
+public class 19-Jan {
+    private int minFallingPathSum(int[][] A, int row, int col, int[][] dp) {
+        int m = A.length;
+        int n = A[0].length;
+
+        if (dp[row][col] != Integer.MAX_VALUE) return dp[row][col];
+
+        if (row == m - 1)
+            return dp[row][col] = A[row][col];
+
+        int left = Integer.MAX_VALUE, right = Integer.MAX_VALUE;
+
+        if (col > 0)
+            left = minFallingPathSum(A, row + 1, col - 1, dp);
+
+        int straight = minFallingPathSum(A, row + 1, col, dp);
+
+        if (col < n - 1)
+            right = minFallingPathSum(A, row + 1, col + 1, dp);
+
+        dp[row][col] = Math.min(left, Math.min(straight, right)) + A[row][col];
+
+        return dp[row][col];
+    }
+
+
+
+    public int minFallingPathSum(int[][] A) {
+        int m = A.length;
+        int n = A[0].length;
+
+        if (m == 1 || n == 1) return A[0][0];
+
+        int[][] dp = new int[m][n];
+        for (int[] row : dp) {
+            Arrays.fill(row, Integer.MAX_VALUE);
+        }
+        
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < A.length; ++i) {
+            ans = Math.min(ans, minFallingPathSum(A, 0, i, dp));
+        }
+
+        return ans;
+    }
+}
