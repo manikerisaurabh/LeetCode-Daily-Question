0b324011879134b87893700e04dddc95446454b4
manikerisaurabh <manikerisaurabh@gmail.com>
2024-02-23 21:43:14 +0530

    23-Feb :+1:

---
+++ 2024/Feb-2024/22-Feb.java
@@ -0,0 +1,52 @@
+/*
+======================================================  997. Find the Town Judge   ======================================================
+
+
+Problem Statement : In a town, there are n people labeled from 1 to n. There is a rumor that one of these people is secretly the town judge.
+
+If the town judge exists, then:
+                            The town judge trusts nobody.
+                            Everybody (except for the town judge) trusts the town judge.
+                            There is exactly one person that satisfies properties 1 and 2.
+                            You are given an array trust where trust[i] = [ai, bi] representing that the person 
+                            labeled ai trusts the person labeled bi. If a trust relationship does not exist in trust array, 
+                            then such a trust relationship does not exist.
+Return the label of the town judge if the town judge exists and can be identified, or return -1 otherwise.
+
+
+Examples : 
+
+                    Example 1:
+                    Input: n = 2, trust = [[1,2]]
+                    Output: 2
+
+
+                    Example 2:
+                    Input: n = 3, trust = [[1,3],[2,3]]
+                    Output: 3
+
+
+                    Example 3:
+                    Input: n = 3, trust = [[1,3],[2,3],[3,1]]
+                    Output: -1
+*/
+
+
+public class 22-Feb {
+    public int findJudge(int n, int[][] trust) {
+        int[] trusted = new int[n + 1];
+
+        for (int[] pair : trust) {
+            trusted[pair[0]] -= 1;
+            trusted[pair[1]] += 1;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (trusted[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;        
+    }
+}
---
+++ 2024/Feb-2024/23-Feb.java
@@ -0,0 +1,59 @@
+/*
+====================================================== 787. Cheapest Flights Within K Stops    ======================================================
+
+
+Problem Statement : There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
+
+
+Examples : 
+
+Example 1:
+
+
+Input: n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1
+Output: 700
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 3 is marked in red and has cost 100 + 600 = 700.
+Note that the path through cities [0,1,2,3] is cheaper but is invalid because it uses 2 stops.
+
+
+Example 2:
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+Output: 200
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 2 is marked in red and has cost 100 + 100 = 200.
+
+
+Example 3:
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+Output: 500
+Explanation:
+The graph is shown above.
+The optimal path with no stops from city 0 to 2 is marked in red and has cost 500.
+*/
+
+
+
+public class 23-Feb {
+    public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
+        int[] dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        dp[src] = 0;
+
+        for (int i = 0; i <= k; i++) {
+            int[] temp = Arrays.copyOf(dp, n);
+            for (int[] flight : flights) {
+                if (dp[flight[0]] != Integer.MAX_VALUE) {
+                    temp[flight[1]] = Math.min(temp[flight[1]], dp[flight[0]] + flight[2]);
+                }
+            }
+            dp = temp;
+        }
+        
+        return dp[dst] == Integer.MAX_VALUE ? -1 : dp[dst];
+    }
+}
