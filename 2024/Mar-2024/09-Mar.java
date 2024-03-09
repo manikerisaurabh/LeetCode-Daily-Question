f20abb01c9393f2d66022bb129b14cbc94d78764
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-09 22:24:12 +0530

    09-Mar :+1:

---
+++ 2024/Mar-2024/09-Mar.java
@@ -0,0 +1,15 @@
+public int getCommon(int[] nums1, int[] nums2) {
+
+    Set<Integer> set = new HashSet<>();
+
+    for (int e : nums1) {
+        set.add(e);
+    }
+
+    for (int ans : nums2) {
+        if (set.contains(ans)) {
+            return ans;
+        }
+    }
+    return -1;
+}
