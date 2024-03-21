a89b49cb407c2779e839e25c85a3c8718fa6d2cb
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-21 22:06:14 +0530

    21-Mar :+1:

---
+++ 2024/Mar-2024/21-Mar.java
@@ -0,0 +1,15 @@
+public class 21-Mar {
+    public ListNode reverseList(ListNode head) {
+        
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while(curr != null) {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
