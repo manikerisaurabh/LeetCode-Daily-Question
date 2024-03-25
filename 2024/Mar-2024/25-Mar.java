8b0ef1652643c17cd0559ce4bb59066a5ac9171f
manikerisaurabh <manikerisaurabh@gmail.com>
2024-03-25 12:16:28 +0530

    25-Mar :+1:

---
+++ 2024/Mar-2024/25-Mar.java
@@ -0,0 +1,15 @@
+public class 25-Mar {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> ans = new ArrayList<>();
+        for(int n: nums) {
+            n = Math.abs(n);
+            if(nums[n - 1] > 0) {
+                nums[n-1] *= -1;
+            }
+            else {
+                ans.add(n);
+            }
+        }
+        return ans;
+    }
+}
