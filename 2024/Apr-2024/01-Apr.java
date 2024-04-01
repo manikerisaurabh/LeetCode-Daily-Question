af864d6563b7a136ed5befd22f2c8e65a6e34fdf
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-01 23:52:41 +0530

    01-Apr :+1:

---
+++ 2024/Apr-2024/01-Apr.java
@@ -0,0 +1,17 @@
+
+public class 01-Apr {
+    
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        int length = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != ' ') {
+                length++;
+            }
+            else if (length > 0) {
+                break;
+            }
+        }
+        return length;
+    }
+}
