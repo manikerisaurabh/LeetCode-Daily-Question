020f352dbd770eb7d616678cdf9b9397df3d0fcf
manikerisaurabh <manikerisaurabh@gmail.com>
2024-04-02 18:12:34 +0530

    02-Apr :+1:

---
+++ 2024/Apr-2024/02-Apr.java
@@ -0,0 +1,30 @@
+public class 02-Apr {
+    public boolean isIsomorphic(String s, String t) {
+        
+        if(s.length() != t.length()) return false;
+
+        Map<Character, Character> map = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+
+            char original = s.charAt(i);
+            char replace = t.charAt(i);
+
+            if(!map.containsKey(original)) {
+                if(!map.containsValue(replace)) {
+                    map.put(original, replace);
+                }
+                else {
+                    return false;
+                }
+            }
+
+            else {
+                char mappedChar = map.get(original);
+                if(mappedChar != replace) return false;
+            }
+
+        }
+        return true;
+    }
+}
