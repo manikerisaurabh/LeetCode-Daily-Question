3ca86097648768a0f4bba3a0da5a96e8c7fa0dd9
manikerisaurabh <manikerisaurabh@gmail.com>
2024-05-02 14:49:52 +0530

    02-Apr :+1:

---
+++ 2024/May-2024/02-May.java
@@ -0,0 +1,15 @@
+public class 02-May {
+    public int findMaxK(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        int maxNum = -1;
+
+        for(int num : nums){
+            if(set.contains(-num))
+                maxNum = Math.max(maxNum, Math.abs(num));
+                
+            set.add(num);
+        }
+
+        return maxNum;
+    }
+}
