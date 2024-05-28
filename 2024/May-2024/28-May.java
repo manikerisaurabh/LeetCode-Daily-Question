8090919ed6e57bd5fc572d2bd27a9de3930b44cc
manikerisaurabh <manikerisaurabh@gmail.com>
2024-05-28 21:56:56 +0530

    28-May :+1:

---
+++ 2024/May-2024/28-May.java
@@ -0,0 +1,14 @@
+public class 28-May {
+    public int equalSubstring(String s, String t, int maxCost) {
+        int res = 0, curr = 0, n = s.length();
+        int[] costs = new int[n];
+        char[] str = s.toCharArray(), target = t.toCharArray();
+        for (int i = 0; i < n; i++) costs[i] = Math.abs(str[i] - target[i]);
+        for (int l = 0, r = 0; r < n; r++){
+            curr += costs[r];
+            curr -= curr > maxCost ? costs[l++] : 0;
+            res = Math.max(res, r - l + 1);
+        }
+        return res;
+    }
+}
