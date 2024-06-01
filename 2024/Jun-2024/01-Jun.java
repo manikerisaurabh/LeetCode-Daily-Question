a1b348b6cab83a7a97b26bf8113275081b15b886
manikerisaurabh <manikerisaurabh@gmail.com>
2024-06-01 12:34:51 +0530

    01-Jun :+1:

---
+++ 2024/Jun-2024/01-Jun.java
@@ -0,0 +1,12 @@
+
+public class 01-Jun {
+    public int scoreOfString(String s) {
+        
+        int ans = 0;
+
+        for(int i = 0; i < s.length()-1; i++) {
+            ans += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return ans;
+    }
+}
