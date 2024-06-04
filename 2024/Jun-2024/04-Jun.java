ba534f0933cbc4b248aa635b6aae20ecce8f7d61
manikerisaurabh <manikerisaurabh@gmail.com>
2024-06-04 19:45:41 +0530

    04-June  :+1:

---
+++ 2024/Jun-2024/04-Jun.java
@@ -0,0 +1,25 @@
+public class 04-Jun {
+    public int longestPalindrome(String s) {
+        Map<Character, Integer> counts = new HashMap<>();
+        
+        for (char c : s.toCharArray()) {
+            counts.put(c, counts.getOrDefault(c, 0) + 1);
+        }
+        
+        int length = 0;
+        boolean hasOdd = false;
+        
+        for (int count : counts.values()) {
+            length += (count / 2) * 2;
+            if (count % 2 == 1) {
+                hasOdd = true;
+            }
+        }
+        
+        if (hasOdd) {
+            length += 1;
+        }
+        
+        return length;
+    }
+}
