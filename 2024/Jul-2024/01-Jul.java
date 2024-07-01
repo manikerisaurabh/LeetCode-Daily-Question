30a8b31a9f57430a0988d839e6742ed8ab921824
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-01 20:44:51 +0530

    01-July :+1:

---
+++ 2024/Jul-2024/01-Jul.java
@@ -0,0 +1,17 @@
+
+public class 01-Jul {
+    public boolean threeConsecutiveOdds(int[] arr) {
+        int count = 0;
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                count++;
+                if (count == 3) {
+                    return true;
+                }
+            } else {
+                count = 0;
+            }
+        }
+        return false;
+    }
+}
