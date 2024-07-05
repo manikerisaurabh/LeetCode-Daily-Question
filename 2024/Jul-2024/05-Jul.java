398770589981eac385c8ac47900f14ac98796fa6
manikerisaurabh <manikerisaurabh@gmail.com>
2024-07-05 17:59:24 +0530

    05-July :+1:

---
+++ 2024/Jul-2024/05-Jul.java
@@ -0,0 +1,37 @@
+public class 05-Jul {
+    public int[] nodesBetweenCriticalPoints(ListNode head) {
+        ListNode slow = head;
+        ListNode med = slow.next;
+        ListNode fast = med.next;
+        int[] arr = new int[2];
+        arr[0]=-1;
+        arr[1]=-1;
+        int minc=0,maxc=0,min=Integer.MAX_VALUE,max=0;
+        boolean a = false;
+        while(fast!=null){
+            if((med.val>slow.val && med.val>fast.val) || (med.val<slow.val && med.val<fast.val)){
+                a=true;
+                if(maxc>max){
+                    max=maxc;
+                }
+                if(minc!=0 && minc<min){
+                    min=minc;
+                }
+                minc=0;
+            }
+            if(a==true){
+                maxc++;
+                minc++;
+            }
+            slow=med;
+            med = fast;
+            fast = fast.next;
+        }
+        if(max==0){
+            return arr;
+        }
+        arr[0]=min;
+        arr[1]=max;
+        return arr;
+    }
+}
